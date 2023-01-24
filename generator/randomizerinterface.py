e1e65b9aa10af3654efbe31cc09b8f28429305bc
Pseudoarc <indecomposable@gmail.com>
2023-01-24 16:28:11 -0500

    Add new flags to options and cosmetic options.

--- generator/randomizerinterface.py
+++ generator/randomizerinterface.py
@@ -153,17 +153,20 @@ class RandomizerInterface:
         :param form: RomForm with cosmetic settings
         """
         # Cosmetic settings
-        if form.cleaned_data['reduce_flashes']:
-            settings.cosmetic_flags = settings.cosmetic_flags | rset.CosmeticFlags.REDUCE_FLASH
+        cos_flag_dict: dict[str, rset.CosmeticFlags] = {
+            'reduce_flashes': rset.CosmeticFlags.REDUCE_FLASH,
+            'zenan_alt_battle_music': rset.CosmeticFlags.ZENAN_ALT_MUSIC,
+            'death_peak_alt_music': rset.CosmeticFlags.DEATH_PEAK_ALT_MUSIC,
+            'quiet_mode': rset.CosmeticFlags.QUIET_MODE,
+            'auto_run': rset.CosmeticFlags.AUTORUN
+        }
 
-        if form.cleaned_data['zenan_alt_battle_music']:
-            settings.cosmetic_flags = settings.cosmetic_flags | rset.CosmeticFlags.ZENAN_ALT_MUSIC
+        cos_flags = rset.CosmeticFlags(False)
+        for name, flag in cos_flag_dict.items():
+            if form.cleaned_data[name]:
+                cos_flags |= flag
 
-        if form.cleaned_data['death_peak_alt_music']:
-            settings.cosmetic_flags = settings.cosmetic_flags | rset.CosmeticFlags.DEATH_PEAK_ALT_MUSIC
-
-        if form.cleaned_data['quiet_mode']:
-            settings.cosmetic_flags = settings.cosmetic_flags | rset.CosmeticFlags.QUIET_MODE
+        settings.cosmetic_flags = cos_flags
 
         # Character/Epoch renames
         settings.char_names[0] = self.get_character_name(form.cleaned_data['crono_name'], 'Crono')
@@ -271,51 +274,58 @@ class RandomizerInterface:
         # techs
         settings.techorder = tech_order_map[form.cleaned_data['tech_rando']]
 
-        if form.cleaned_data['disable_glitches']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.FIX_GLITCH
+        GF = rset.GameFlags
+        gameflags_dict = {
+            # Main
+            'disable_glitches': GF.FIX_GLITCH,
+            'boss_rando': GF.BOSS_RANDO,
+            'boss_scaling': GF.BOSS_SCALE,
+            'zeal': GF.ZEAL_END,
+            'early_pendant': GF.FAST_PENDANT,
+            'locked_chars': GF.LOCKED_CHARS,
+            'unlocked_magic': GF.UNLOCKED_MAGIC,
+            'tab_treasures': GF.TAB_TREASURES,
+            'chronosanity': GF.CHRONOSANITY,
+            'duplicate_characters': GF.DUPLICATE_CHARS,
+            'healing_item_rando': GF.HEALING_ITEM_RANDO,
+            'gear_rando': GF.GEAR_RANDO,
+            'mystery_seed': GF.MYSTERY,
+            'epoch_fail': GF.EPOCH_FAIL,
+            'duplicate_duals': GF.DUPLICATE_TECHS,
+            # This should get moved to ROSettings.
+            'boss_spot_hp': GF.BOSS_SPOT_HP,
+            # Extra
+            'unlocked_skyways': GF.UNLOCKED_SKYGATES,
+            'add_sunkeep_spot': GF.ADD_SUNKEEP_SPOT,
+            'add_bekkler_spot': GF.ADD_BEKKLER_SPOT,
+            'add_cyrus_spot': GF.ADD_CYRUS_SPOT,
+            'restore_tools': GF.RESTORE_TOOLS,
+            'add_ozzie_spot': GF.ADD_OZZIE_SPOT,
+            'restore_johnny_race': GF.RESTORE_JOHNNY_RACE,
+            'add_racelog_spot': GF.ADD_RACELOG_SPOT,
+            'split_arris_dome': GF.SPLIT_ARRIS_DOME,
+            'vanilla_robo_ribbon': GF.VANILLA_ROBO_RIBBON,
+            'vanilla_desert': GF.VANILLA_DESERT,
+            'use_antilife': GF.USE_ANTILIFE,
+            'tackle_effects': GF.TACKLE_EFFECTS_ON,
+            'starters_sufficient': GF.STARTERS_SUFFICIENT,
+            'bucket_list': GF.BUCKET_LIST,
+            # QoL
+            'sightscope_always_on': GF.VISIBLE_HEALTH,
+            'boss_sightscope': GF.BOSS_SIGHTSCOPE,
+            'fast_tabs': GF.FAST_TABS,
+            'free_menu_glitch': GF.FREE_MENU_GLITCH,
+        }
 
-        if form.cleaned_data['boss_rando']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.BOSS_RANDO
-
-        if form.cleaned_data['boss_scaling']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.BOSS_SCALE
-
-        if form.cleaned_data['zeal']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.ZEAL_END
-
-        if form.cleaned_data['early_pendant']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.FAST_PENDANT
-
-        if form.cleaned_data['locked_chars']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.LOCKED_CHARS
-
-        if form.cleaned_data['unlocked_magic']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.UNLOCKED_MAGIC
-
-        if form.cleaned_data['tab_treasures']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.TAB_TREASURES
-
-        if form.cleaned_data['chronosanity']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.CHRONOSANITY
-
-        if form.cleaned_data['duplicate_characters']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.DUPLICATE_CHARS
-
-        if form.cleaned_data['healing_item_rando']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.HEALING_ITEM_RANDO
-
-        if form.cleaned_data['gear_rando']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.GEAR_RANDO
-
-        if form.cleaned_data['mystery_seed']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.MYSTERY
-
-        if form.cleaned_data['epoch_fail']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.EPOCH_FAIL
+        settings.gameflags = GF(False)
+        for name, flag in gameflags_dict.items():
+            if form.cleaned_data[name]:
+                settings.gameflags |= flag
 
         # Duplicate characters
         char_choices = []
-        duplicate_char_assignments = form.cleaned_data['duplicate_char_assignments']
+        duplicate_char_assignments = \
+            form.cleaned_data['duplicate_char_assignments']
         # duplicate character assignments comes in as a stringified hex number.
         # Decode the hex string into the char_choices list.
         # Loop through the characters
@@ -328,15 +338,14 @@ class RandomizerInterface:
                     char_choices[i].append(j)
         settings.char_choices = char_choices
 
-        if form.cleaned_data['duplicate_duals']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.DUPLICATE_TECHS
-
         # Boss rando settings
         # TODO - Boss and location lists are just default for now. Only update the other options.
-        settings.ro_settings.preserve_parts = form.cleaned_data['legacy_boss_placement']
+        settings.ro_settings.flags = rset.ROFlags(False)
+        if form.cleaned_data['legacy_boss_placement']:
+            settings.ro_settings.flags |= rset.ROFlags.PRESERVE_PARTS
 
         if form.cleaned_data['boss_spot_hp']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.BOSS_SPOT_HP
+            settings.ro_settings.flags |= rset.ROFlags.BOSS_SPOT_HP
 
         # Tab randomization settings
         # TODO - Currently defaulting to UNIFORM distribution
@@ -351,23 +360,7 @@ class RandomizerInterface:
             speed_max=form.cleaned_data['speed_tab_max']
         )
 
-        # Quality of life settings
-        if form.cleaned_data['sightscope_always_on']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.VISIBLE_HEALTH
-
-        if form.cleaned_data['boss_sightscope']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.BOSS_SIGHTSCOPE
-
-        if form.cleaned_data['fast_tabs']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.FAST_TABS
-
-        if form.cleaned_data['free_menu_glitch']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.FREE_MENU_GLITCH
-
-        # Bucket List
-        if form.cleaned_data['bucket_list']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.BUCKET_LIST
-
+        # Bucket Settings
         disable_other_go_modes = form.cleaned_data['bucket_disable_go_modes']
         objectives_win = form.cleaned_data['bucket_obj_win_game']
         num_objectives = form.cleaned_data['bucket_num_objs']
@@ -385,16 +378,6 @@ class RandomizerInterface:
             num_objectives_needed=num_objectives_needed,
             hints=hints
         )
-
-        # Extra settings
-        if form.cleaned_data['use_antilife']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.USE_ANTILIFE
-
-        if form.cleaned_data['tackle_effects']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.TACKLE_EFFECTS_ON
-
-        if form.cleaned_data['starters_sufficient']:
-            settings.gameflags = settings.gameflags | rset.GameFlags.STARTERS_SUFFICIENT
 
         # Mystery
         settings.mystery_settings.game_mode_freqs: dict[rset.GameMode, int] = {
@@ -482,7 +465,10 @@ class RandomizerInterface:
         return spoiler_log
 
     @staticmethod
-    def get_web_spoiler_log(config: randoconfig.RandoConfig) -> dict[str, list[dict[str, str]]]:
+    def get_web_spoiler_log(
+            # settings: rset.Settings,
+            config: randoconfig.RandoConfig
+    ) -> dict[str, list[dict[str, str]]]:
         """
         Get a dictionary representing the spoiler log data for the given seed.
 
@@ -493,7 +479,20 @@ class RandomizerInterface:
             'characters': [],
             'key_items': [],
             'bosses': []
+            # 'objectives': []
         }
+
+        # if False and rset.GameFlags.BUCKET_LIST in settings.gameflags:
+        #     num_objs = settings.bucket_settings.num_objectives
+
+        #     for ind, objective in enumerate(config.objectives):
+        #         if ind >= num_objs:
+        #             break
+
+        #         spoiler_log['objectives'].append(
+        #             {'name': str(f"Objective {ind}"),
+        #              'desc': objective.desc}
+        #         )
 
         # Character data
         for recruit_spot in config.char_assign_dict.keys():
