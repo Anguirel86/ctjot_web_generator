65d069e4b7a9c350ab17afc3c1bd61f7258f6e93
C <caltronnotlrac@gmail.com>
2023-08-02 11:37:01 -0400

    enable saving cosmetics to cookie for re-use between seeds
    Adds ability to push button under Cosmetics Options to save cosmetics
    settings to a cookie ("ctjot_cosmetics"). When that cookie is present,
    when seed generated, values are read from the cookie and pre-populate
    settings on the seed share page. Also adds ability to explicitly re-load
    from cookie and reset to defaults (which also removes cookie).

--- generator/static/generator/seed.js
+++ generator/static/generator/seed.js
@@ -17,18 +17,114 @@ $(document).on('hide.bs.collapse', '#cosmetic_section', function(e) {
 });
 
 /*
+ * Get cosmetic options from json-script data.
+ */
+function getCosmeticOptions() {
+  return JSON.parse(document.getElementById('cosmetics-options').textContent);
+}
+
+/*
  * Update the game options section of the seed page.
  */
 function updateGameOptions() {
+  let options = getCosmeticOptions();
 
   // Update the slider text boxes
-  var id_list = ['battle_speed', 'battle_message_speed', 'battle_gauge_style', 'background_selection']
-  for (const id of id_list) {
-    document.getElementById(id + "_text").value = document.getElementById("id_" + id).value
-  }
+  Object.entries(options).forEach(([id, option]) => {
+    if (option.type == 'slider') {
+      document.getElementById(id + "_text").value = document.getElementById("id_" + id).value;
+    }
+  });
 
   // Display the chosen background type.
   var selection = document.getElementById('id_background_selection').value;
   preview = document.getElementById('background_selection_preview');
   preview.className = 'menuBackground' + selection;
+}
+
+/*
+ * Load cosmetic options from cookie.
+ */
+function loadCosmeticOptionsCookie() {
+  // load cookie
+  let cookie = Cookies.get('ctjot_cosmetics');
+  if (!cookie) { return; }
+
+  try {
+    var cosmetics = JSON.parse(atob(cookie));
+  } catch (e) {
+    console.warn('Failed to parse ctjot_cosmetics cookie.', {name: e.name, message: e.message});
+  }
+  if (!cosmetics) { return; }
+
+  // update items from cookie
+  let options = getCosmeticOptions();
+  Object.entries(options).forEach(([id, option]) => {
+    let elem = document.getElementById("id_" + id);
+    if (elem) {
+      if (option.type == 'checked') {
+        let checked = cosmetics[id];
+        if (checked) { $('#id_' + id).bootstrapToggle('on'); }
+        else { $('#id_' + id).bootstrapToggle('off'); }
+      } else {
+        elem.value = cosmetics[id];
+      }
+    } else {
+      console.warn('Undefined cosmetics element.', {id: id});
+    }
+  });
+
+  updateGameOptions();
+}
+
+/*
+ * Save cosmetic options to cookie.
+ */
+function saveCosmeticOptionsCookie() {
+  var cosmetics = {};
+  let options = getCosmeticOptions();
+
+  // read form values into object
+  Object.entries(options).forEach(([id, option]) => {
+    if (option.type == 'checked') {
+      let checked = $('#id_' + id).is(':checked');
+      cosmetics[id] = (checked) ? true : false;
+    } else {
+      cosmetics[id] = document.getElementById("id_" + id).value;
+    }
+  });
+
+  // stringify and base64 encode JSON then write cookie
+  try {
+    let cookie = btoa(JSON.stringify(cosmetics));
+    Cookies.set('ctjot_cosmetics', cookie, { sameSite: 'lax' });
+  } catch(e) {
+    console.warn('Failed to write ctjot_cosmetics cookie.', {name: e.name, message: e.message});
+  }
+}
+
+/*
+ * Reset cosmetic options to defaults and clear cookie.
+ */
+function resetCosmeticOptionsCookie() {
+  let options = getCosmeticOptions();
+  Object.entries(options).forEach(([id, option]) => {
+    let elem = document.getElementById("id_" + id);
+    if (elem) {
+      if (option.type == 'checked') {
+        let checked = option.default;
+        if (checked) { $('#id_' + id).bootstrapToggle('on'); }
+        else { $('#id_' + id).bootstrapToggle('off'); }
+      } else {
+        elem.value = option.default;
+      }
+    } else {
+      console.warn('Undefined cosmetics element.', {id: id});
+    }
+  });
+
+  // delete cookie
+  Cookies.remove('ctjot_cosmetics');
+
+  updateGameOptions();
 }
