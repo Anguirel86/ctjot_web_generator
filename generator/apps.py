5ab4e231c3deeeeb4585a29e77f02d613b3b8e9f
C <caltronnotlrac@gmail.com>
2023-08-18 10:05:07 -0400

    in debug, use MD5 sums of static files to cache-bust
    Alternative implementation, via monkeypatching 'static' template
    tag.
    
    This reverts commit f4c6c588a0f15a16dcd63d206c04f7d17253f494.

--- generator/apps.py
+++ generator/apps.py
@@ -1,9 +1,10 @@
 import os
 
+from urllib.parse import quote, urljoin
 from collections import defaultdict
 from hashlib import md5
 from pathlib import Path
-from typing import DefaultDict
+from typing import Dict
 
 from django.apps import AppConfig
 
@@ -12,22 +13,42 @@ class GeneratorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'generator'
 
-    js_debug_query: DefaultDict[str, str] = defaultdict(lambda: '')
+    def __init__(self, app_name, app_module):
+        if bool(int(os.getenv("DEBUG", "0"))):
+            self._monkey_patch_static_node()
 
-    # when DEBUG is not set, django is using ManifestStaticFilesStorage
-    # to automatically manage hashing of static files and replacing
-    # in templates, however, we want DEBUG in some settings like dev,
-    # so this alternative method can be used there
-    if bool(int(os.getenv("DEBUG", "0"))):
-        # map truncated path to each static .js file to it's MD5 checksum
-        # to use as a "version" query for cache-busting
-        # so browser consistently reloads js files on changes
-        js_debug_query = defaultdict(
-            lambda: '',
-            {
-                str(Path(*path.parts[2:])): (
-                    f"?v={md5(path.read_bytes()).hexdigest()}"
-                )
-                for path in Path('generator/static').rglob('*.js')
-            }
-        )
+        super(GeneratorConfig, self).__init__(app_name, app_module)
+
+    @staticmethod
+    def _monkey_patch_static_node():
+        '''
+        Updates 'static' template tag to append a query string on static
+        assets to compel browser to consistently reload the updated files.
+
+        When DEBUG is not set, django is using ManifestStaticFilesStorage
+        to automatically manage hashing of static files and replacing
+        in templates. However, we want DEBUG in some settings like dev,
+        so use this alternative method there.
+        '''
+        # map truncated path to each static file to it's MD5 checksum
+        md5sums : Dict[str, str] =  {
+            str(Path(*path.parts[2:])): md5(path.read_bytes()).hexdigest()
+            for path in Path('generator/static').rglob('*')
+            if path.is_file()
+        }
+
+        # monkeypatch the 'static' template tag to automatically
+        # append a "?v={md5sum}" query on static files
+        # to cache-bust browser caching when the files are updated
+        from django.templatetags.static import PrefixNode, StaticNode
+
+        def handle_simple_cache_bust(cls, path):
+            quoted_path = quote(path)
+
+            md5sum = md5sums.get(path)
+            if md5sum:
+                quoted_path += f"?v={md5sum}"
+
+            return urljoin(PrefixNode.handle_simple("STATIC_URL"), quoted_path)
+
+        StaticNode.handle_simple = classmethod(handle_simple_cache_bust)
